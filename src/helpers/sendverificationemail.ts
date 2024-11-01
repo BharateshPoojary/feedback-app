1efdbee9eada08af6ca82f39bbab681ad0a742fe
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-01 14:48:30 +0530

    emailverification code done

--- src/helpers/sendverificationemail.ts
+++ src/helpers/sendverificationemail.ts
@@ -2,3 +2,23 @@ import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/verification-email-template";
 //we can use any syntax it can be @ or .. one
 import { ApiResponse } from "@/types/ApiResponse";
+
+export const sendverificationemail = async (
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> => {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev", //as we are using free tier account
+      to: email,
+      subject: "Bharat-Feedback-app | Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+      /**So otp is the prop name expected by VerificationEmail, and verifyCode is the actual value being assigned to it */
+    });
+    return { success: true, message: "verification email sent successfully" };
+  } catch (emailError) {
+    console.log("Error sending verification email");
+    return { success: false, message: "Error sending verification email" };
+  }
+};
