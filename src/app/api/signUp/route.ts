9a1a66e6381c3c2558f6c61d2a915bd4d30d1f89
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-02 15:33:30 +0530

    signup route done

--- src/app/api/signUp/route.ts
+++ src/app/api/signUp/route.ts
@@ -12,6 +12,72 @@ Request is a type, often provided by libraries or frameworks (like Express, Next
   try {
     const { username, email, password } = await request.json();
     /**This part waits for the JSON content of the incoming request to be parsed. The request.json() method reads the body of the request as JSON, which is typically used when the request's body contains JSON data (like {"username": "example", "email": "example@example.com", "password": "securepassword"}). */
+    const isExisting_user_by_this_username = await UserModel.findOne({
+      username: username,
+    });
+    //if  user exist by this username
+    if (isExisting_user_by_this_username) {
+      return Response.json(
+        { success: false, message: "Username already exists" },
+        { status: 409 }
+      );
+    }
+    const Existing_user_by_this_email = await UserModel.findOne({
+      email: email,
+    });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const ExpiryDate = new Date();
+    ExpiryDate.setHours(ExpiryDate.getHours() + 1);
+    //if a user exist by this email
+    if (Existing_user_by_this_email) {
+      //if a user exist by this email and is verified
+      if (Existing_user_by_this_email.isVerified) {
+        return Response.json(
+          { success: false, message: "User with this email already exists" },
+          { status: 409 }
+        );
+      } else {
+        //if a user exist by this email and is  not verified which means he is updating  his password
+        const hashedpasswordfornewpassword = await bcrypt.hash(password, 10);
+        Existing_user_by_this_email.password = hashedpasswordfornewpassword;
+        Existing_user_by_this_email.verifyCode = verifyCode;
+        Existing_user_by_this_email.verifyCodeExpiry = ExpiryDate;
+      }
+    } else {
+      //if a user does not exist then regsitering a new user
+      const hashedpassword = await bcrypt.hash(password, 10);
+      const creatingnewuser = new UserModel({
+        username: username,
+        useremail: email,
+        password: hashedpassword,
+        verifyCode: verifyCode,
+        verifyCodeExpiry: ExpiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        message: [],
+      });
+      await creatingnewuser.save();
+    }
+
+    const verificationemail = await sendverificationemail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!verificationemail.success) {
+      return Response.json(
+        { success: false, message: verificationemail.message },
+        { status: 500 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: verificationemail.message,
+        },
+        { status: 201 }
+      );
+    }
   } catch (error) {
     console.error("Error registering user", error);
     return Response.json(
