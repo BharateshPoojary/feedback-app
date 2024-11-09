2ed6fe76767f0ca742311ced3896a00f0eb7ee54
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-09 22:20:56 +0530

    installing shadcn for frontend components

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+//all the classes that the tailwind css uses are merged here
