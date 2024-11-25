e32adfedbdf3e7583c6d20f9539f952fc3a6c60d
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-25 22:33:05 +0530

    dashboard creation in progress

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useSession } from "next-auth/react";
+import { z } from "zod";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 const page = () => {
+  const { data: session } = useSession();
+  const form = useForm<z.infer<typeof acceptMessageSchema>>({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+  const { register, watch, setValue } = form;
   return <div></div>;
 };
-
 export default page;
