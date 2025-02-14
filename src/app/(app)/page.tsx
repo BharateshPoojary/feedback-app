45abf4893ac33803101fd131310a0a2bb9175f8e
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-02-15 00:56:30 +0530

    carousel in pr

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+"use client";
+import EmblaCarousel from "@/app/(app)/EmblaCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+import "../base.css";
+import "../embla.css";
+import "../sandbox.css";
 
 const Home = () => {
+  const OPTIONS: EmblaOptionsType = {};
+  const SLIDE_COUNT = 5;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+  // console.log(SLIDES);
   return (
     <div>
       <h2 className="text-center text-2xl md:text-5xl my-5 font-bold">
@@ -9,6 +18,9 @@ const Home = () => {
       <h5 className="text-center text-lg md:text-3xl my-5 font-bold">
         Get your first anonymous message now !!
       </h5>
+      <>
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </>
     </div>
   );
 };
